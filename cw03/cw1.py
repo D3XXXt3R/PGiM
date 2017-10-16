4bdbdfc34833fa958346fd6f64c5e649316975c0
D3XXXt3R <pawelkalecinski94@gmail.com>
2017-10-16 13:18:40 +0200

    PGiM_Z2/PGiM_Z3

---
+++ cw03/cw1.py
@@ -0,0 +1,13 @@
+import cv2
+
+def incr_pixel(val):
+    img3 = cv2.imread('lena.jpg',1)
+    for i in img3:
+        for j in i:
+            j += val
+    return img3
+
+cv2.namedWindow('image2', cv2.WINDOW_NORMAL)
+cv2.imshow('image2', incr_pixel(50))
+cv2.waitKey(0)
+cv2.destroyAllWindows()
